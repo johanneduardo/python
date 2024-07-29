b4806529a92a5be2c4ea07abab5e5bbbcb449c41
main <johanneduardo18@gmail.com>
2024-07-29 20:55:17 -0300

    commitando mais doi arquivos

---
+++ listas.py
@@ -0,0 +1,8 @@
+nomes = []
+while True:
+    nome = input ("Digite -1 para sair ou cadastre-se com um nome: ")
+    if nome == '-1':
+        break
+    nomes.append(nomes)
+
+print(nomes)
---
+++ listas2.py
@@ -0,0 +1,11 @@
+#listas podem ter o metodo append ou insert, insert oce pode escolher onde vai tal coisa, e append apenas adiciona#
+#pop serve para apagar a pessoa atraves do index e tambem temos o remove para apagar pessoas atraves do nome#
+#sort serve para deixar na ordem alfabetica#
+
+
+nomes = [3,5,27,59,2,1,6,8,5,2,4,5,2,1,2,3,125,3,25,8]
+
+nomes.sort()
+
+print (nomes)
+

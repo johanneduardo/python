59168e6f298393141b5e1fabd8beaa442a376cb5
Johann Eduardo <johanneduardo18@gmail.com>
2024-09-25 22:45:40 -0300

    Adicionando o projeto de advinhar numero

---
+++ projetosdecurso/listassoma.py
@@ -0,0 +1,11 @@
+
+#nesse caso o X recebe valores, que sao armazenados dentro de uma lista
+x = [int(input()) for i in range (0,10)]
+
+# 'y' é uma variável acumuladora, onde seu valor inicial é 0. Ela vai armazenar a soma dos valores de 'x'. (GPT)
+y = 0 
+
+#variavel i é criada para ir fazndo tambem iteração com x, onde I ira percorrer a lista e ira somando com o valor Y
+for i in x :
+    y += + i
+print (y)
